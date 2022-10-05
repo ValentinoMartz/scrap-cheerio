769a29cdea19639ef915ead5c23e2df9e48053f8
ValentinoMartz <valentinomartinez55@gmail.com>
2022-10-05 14:38:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const PORT = 8000;
+
+const app = express();
+const url = "https://www.theguardian.com/uk";
+
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articules = [];
+    $(".fc-item__title", html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articules.push({
+        title,
+        url,
+      });
+    });
+
+    console.log(articules);
+  })
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
