49d1a3c7c67ff41ec126c045c4e986a464ee8fd5
ValentinoMartz <valentinomartinez55@gmail.com>
2022-10-05 18:51:49 -0300

    done

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
 const PORT = 8000;
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
 const url = "https://www.theguardian.com/uk";
 
-axios(url)
-  .then((response) => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const articules = [];
-    $(".fc-item__title", html).each(function () {
-      const title = $(this).text();
-      const url = $(this).find("a").attr("href");
-      articules.push({
-        title,
-        url,
-      });
-    });
+//app.METHOD(PATH, HANDLER);
 
-    console.log(articules);
-  })
-  .catch((err) => console.log(err));
+app.get("/", async (req, res) => {
+  res.json("This is my webscraper");
+});
+
+app.get("/results", async (req, res) => {
+  axios(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const articules = [];
+      $(".fc-item__title", html).each(function () {
+        const title = $(this).text();
+        const url = $(this).find("a").attr("href");
+        articules.push({
+          title,
+          url,
+        });
+      });
+
+      res.json(articules);
+    })
+    .catch((err) => console.log(err));
+});
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
